cb25c999b168e35a4164d6ee7f393352f4efb73c
Michele Caramello <michele@treasure-data.com>
2016-07-01 16:32:08 -0700

    support alternate webhook for testing and/or routing to diffent slack destinations

--- app.rb
+++ app.rb
@@ -1,25 +1,55 @@
 require 'sinatra'
 require 'json'
 require 'slack-notifier'
+require 'tilt/erubis'
 
-SLACK_NOTIFIER = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+ENV['SLACK_WEBHOOK_URL'] =      'https://hooks.slack.com/services/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'
+ENV['SLACK_WEBHOOK_URL_TEST'] = 'https://hooks.slack.com/services/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'
+
+SLACK_NOTIFIER = {
+  test: Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL_TEST']),
+  default: Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']),
+}
 
 put '/:template' do
-  channel = params[:channel]
   template = params[:template]
+
   begin
-    payload = JSON.parse(request.body.read)  
-    # payload is formatted like
-    # { "column_names": ["foo", "bar"],
+    payload = JSON.parse(request.body.read)
+    # payload is formatted like:
+    # {
+    #   "column_names": ["foo", "bar"],
     #   "data": [[1,2,3], ['a', 'b', 'c']],
     #   "column_type": ["long", "string"]
     # }
     # See http://docs.treasuredata.com/articles/result-into-web
-    @td = Hash[payload['column_names'].zip(payload['data'].transpose)]
+    @td = Hash[payload['column_names'].zip(payload['data'])]
     s = erb template.to_sym, :layout => false
-    SLACK_NOTIFIER.ping(s) 
+
+    slack_notifier = nil
+    if params.has_key?('env')
+      if SLACK_NOTIFIER[params['env'].to_sym]
+        slack_notifier = SLACK_NOTIFIER[params['env'].to_sym]
+      else
+        puts "no slack environment definition for '#{params['env']}'. Falling back to 'default'."
+      end
+    end
+    if slack_notifier.nil?
+      slack_notifier = SLACK_NOTIFIER[:default]
+    end
+    slack_notifier.ping(s)
   rescue => e
     STDERR.puts e.backtrace
   end
+end
+
+get '/' do
+  """
+  <head>
+  </head>
+  <body>
+  <div>Use <tt><strong>PUT</strong> /template_name</tt></div>
+  </body>
+  """
 end
 
